91cc49c39a466db8f6a06eb92713e52c0309eb55
AHReccese <amirhssin6rst@gmail.com>
2024-04-14 18:51:28 +0330

    add personal fields correspondence dict in xml files

--- metadoc/dmeta_param.py
+++ metadoc/dmeta_param.py
@@ -1,3 +1,31 @@
 # -*- coding: utf-8 -*-
-"""Parameters and constants."""
+"""Dmeta Parameters and constants."""
 DMETA_VERSION = "0.1"
+
+PERSONAL_FIELDS_CORE_XML_CORRESPONDENCES = {
+    #Description
+    "title" : "title",
+    "subject": "subject",
+    "tags": "keywords",
+    "categories": "category",
+    "comments": "description",
+
+    #Origin
+    "authors": "creator",
+    "lastSavedBy": "lastModifiedBy",
+    "revisionNumber": "revision",
+    "versionNumber": "version",
+
+    #Content
+    # we can't change "content type" field, it is automatically reset.
+    "contentStatus": "contentStatus",
+    "language": "language",
+}
+
+PERSONAL_FIELDS_APP_XML_CORRESPONDENCES = {
+    #Origin 
+    #we can't change "Application" field, it becomes a corrupted file.
+    "programName": "Application",
+    "company": "Company",
+    "manager": "Manager"
+}
