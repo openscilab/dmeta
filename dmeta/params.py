232d8eda4a258eb69a56770fbdf363cea8f5e908
AHReccese <amirhssin6rst@gmail.com>
2024-04-27 23:20:54 +0330

    add `PERSONAL_FIELDS_CORE_XML_CORRESPONDENCES` params

--- dmeta/params.py
+++ dmeta/params.py
@@ -1,3 +1,23 @@
 # -*- coding: utf-8 -*-
 """Dmeta Parameters and constants."""
 DMETA_VERSION = "0.1"
+
+PERSONAL_FIELDS_CORE_XML_CORRESPONDENCES = {
+    #Description
+    "title" : "title",
+    "subject": "subject",
+    "tags": "keywords",
+    "categories": "category",
+    "comments": "description",
+
+    #Origin
+    "authors": "creator",
+    "lastSavedBy": "lastModifiedBy",
+    "revisionNumber": "revision",
+    "versionNumber": "version",
+
+    #Content
+    # we can't change "content type" field, it is automatically reset.
+    "contentStatus": "contentStatus",
+    "language": "language",
+}
