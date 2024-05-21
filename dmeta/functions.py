16b998f7583b0cc75a5bd6443606e1cf411a8d0c
AHReccese <amirhssin6rst@gmail.com>
2024-05-21 09:15:08 +0330

    replace xml.ET with defusedxml.ET

--- dmeta/functions.py
+++ dmeta/functions.py
@@ -4,7 +4,7 @@ import os
 import shutil
 import zipfile
 from .util import remove_format, extract_docx, extract_namespaces, read_json
-import xml.etree.ElementTree as ET
+import defusedxml.ElementTree as ET
 from .params import CORE_XML_MAP, APP_XML_MAP, OVERVIEW
 
 
@@ -21,15 +21,6 @@ def clear(docx_file_name):
     doc_props_dir = os.path.join(unzipped_dir, "docProps")
     core_xml_path = os.path.join(doc_props_dir, "core.xml")
     app_xml_path = os.path.join(doc_props_dir, "app.xml")
-
-    core_xml_ns = extract_namespaces(core_xml_path)
-    app_xml_ns = extract_namespaces(app_xml_path)
-
-    for key, value in core_xml_ns.items():
-        ET.register_namespace(key, value)
-
-    for key, value in app_xml_ns.items():
-        ET.register_namespace(key, value)
 
     e_core = ET.parse(core_xml_path)
     e_app = ET.parse(app_xml_path)
@@ -99,12 +90,7 @@ def update(config_file_name, docx_file_name):
     core_xml_path = os.path.join(doc_props_dir, "core.xml")
     app_xml_path = os.path.join(doc_props_dir, "app.xml")
 
-    core_xml_ns = extract_namespaces(core_xml_path)
-    app_xml_ns = extract_namespaces(app_xml_path)
-
     if has_core_tags:
-        for key, value in core_xml_ns.items():
-            ET.register_namespace(key, value)
         e_core = ET.parse(core_xml_path)
         for xml_element in e_core.iter():
             for personal_field in personal_fields_core_xml:
@@ -114,8 +100,6 @@ def update(config_file_name, docx_file_name):
         e_core.write(core_xml_path, "utf-8", True, None, "xml")
 
     if has_app_tags:
-        for key, value in app_xml_ns.items():
-            ET.register_namespace(key, value)
         e_app = ET.parse(app_xml_path)
         for xml_element in e_app.iter():
             for personal_field in personal_fields_app_xml:
