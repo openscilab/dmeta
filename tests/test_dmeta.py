084318c957806cbc4cccc2d9085519ca0f2cc2e3
AHReccese <amirhssin6rst@gmail.com>
2025-08-02 17:58:25 -0400

    add test to check `png` metadata removal (inplace, not-inplace)

--- tests/test_dmeta.py
+++ tests/test_dmeta.py
@@ -1,5 +1,7 @@
 import os
+from PIL import Image
 from dmeta.functions import update, update_all, clear, clear_all
+from dmeta.functions import clear_png_metadata
 
 TESTS_DIR_PATH = os.path.join(os.getcwd(), "tests")
 
@@ -45,3 +47,18 @@ def test8():
     # update all existing .docx files [inplace]
     os.chdir(TESTS_DIR_PATH)
     update_all(os.path.join(TESTS_DIR_PATH, "config.json"), in_place=True)
+
+
+def test9():
+    # clear the metadata of the .png file [inplace]
+    png_file = os.path.join(TESTS_DIR_PATH, "test.png")
+    clear_png_metadata(png_file, in_place=True, verbose=False)
+    with Image.open(png_file) as img:
+        assert img.info == {}
+
+def test10():
+    # clear the metadata of the .png file [not inplace]
+    png_file = os.path.join(TESTS_DIR_PATH, "test.png")
+    output_path = clear_png_metadata(png_file, in_place=False, verbose=False)
+    with Image.open(output_path) as img:
+        assert img.info == {}
